0315e9d3d37bf0e55c46b6222f0977f556b8fe02
carlos5845 <martinescarlos154@gmail.com>
2025-02-27 10:30:17 -0500

    modeios y controladores creados

---
+++ app/Http/Controllers/DiaLaborableController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DiaLaborable;
+use Illuminate\Http\Request;
+
+class DiaLaborableController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(DiaLaborable $diaLaborable)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(DiaLaborable $diaLaborable)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, DiaLaborable $diaLaborable)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(DiaLaborable $diaLaborable)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/ResolucionGdhController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ResolucionGdh;
+use Illuminate\Http\Request;
+
+class ResolucionGdhController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ResolucionGdh $resolucionGdh)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ResolucionGdh $resolucionGdh)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ResolucionGdh $resolucionGdh)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ResolucionGdh $resolucionGdh)
+    {
+        //
+    }
+}
